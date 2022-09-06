61350681a4ca0bb7ba7810b872d039366c124312
MyKnu <color.ui>
2022-09-06 22:05:13 +0900

    myCom

--- src/MDbrew copy/RDF.py
+++ src/MDbrew copy/RDF.py
@@ -1,30 +1,34 @@
 import numpy as np
+import pandas as pd
 from tqdm import trange
 import matplotlib.pyplot as plt
+from .tools import timeCount
+
 
 # Calculate and Plot the RDF
 class RDF:
-    def __init__(self, a: np.array, b: np.array, box_size: np.array) -> None:
+    def __init__(self, a: np.array, b: np.array, system_size: np.array) -> None:
         """init
 
         Args:
             a (np.array): [lag time, N_particle, dim]
             b (np.array): [lag time, N_particle, dim]
-            box_size (np.array): [lx, ly, lz]
+            box_size (np.array): [[-lx, lx], [-ly, ly], [-lz, lz]]
         """
-        self.a = a
-        self.b = b
-        self.box_size = box_size
+        self.a = np.asarray(a)
+        self.b = np.asarray(b)
+        self.systme_size = np.asarray(system_size)[:, 1]
+        self.box_length = self.systme_size * 2
         self.lag_number = self.a.shape[0]
         self.a_number = self.a.shape[1]
         self.b_number = self.b.shape[1]
-        self.b_number_density = self.b_number / np.prod(self.box_size)
+        self.b_number_density = self.b_number / np.prod(self.box_length)
         self.BAR_LENGTH = 70
 
     # Main function for Get rdf from a and b, [lag time, N_particle, dim]
     def get_rdf_all(self, resolution: int = 200):
         self.resolution = resolution
-        self.r_max = min(self.box_size) / 2
+        self.r_max = np.min(self.systme_size)
         self.dr = self.r_max / resolution
         self.histo_method = self.__check_method()
         self.hist_data = np.zeros(self.resolution)
@@ -110,8 +114,8 @@ class RDF:
         Returns:
             np.ndarray : value checked delta_coordinates with Periodic Boudary Layer
         """
-        for dim, box_length in enumerate(self.box_size):
-            if delta_coordinates[dim] * 2 > box_length:
+        for dim, box_length in enumerate(self.box_length):
+            if delta_coordinates[dim] > self.systme_size[dim]:
                 delta_coordinates[dim] = box_length - delta_coordinates[dim]
         return delta_coordinates
 
@@ -140,3 +144,27 @@ class RDF:
     def set_BAR_LENGTH(self, width=70):
         self.BAR_LENGTH = width
         print(f"Progress bar length is changed into {width}")
+
+
+@timeCount
+def make_rdf_data(data, type_: list[int] = [1, 2], pos_: list[str] = ["x", "y", "z"]):
+    num_type_ = len(type_)
+    if num_type_ > 2:
+        raise Exception(f"RDF only control the two particle, Not {num_type_}")
+    else:
+        database = data.get_database()
+        column = data.get_columns()
+        lag_number = len(database)
+        database_one = []
+        database_two = []
+        for idx in range(lag_number):
+            df_data = pd.DataFrame(data=database[idx], columns=column)
+            df_one = df_data[df_data["type"] == type_[0]]
+            df_two = df_data[df_data["type"] == type_[-1]]
+            pos_one = df_one[pos_]
+            pos_two = df_two[pos_]
+            database_one.append(pos_one)
+            database_two.append(pos_two)
+        database_one = np.array(database_one)
+        database_two = np.array(database_two)
+        return database_one, database_two
