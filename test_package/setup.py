426d8c73ceac132cc5f93352b74937d37a7847d9
MyKnu <k1alty@naver.com>
2022-09-08 15:39:56 +0900

    minw928

---
+++ test_package/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MDbrew",
+    version="1.1.1",
+    author="Knu",
+    author_email="k1alty@naver.com",
+    url="https://github.com/MyKnu/MDbrew",
+    download_url="https://github.com/MyKnu/install_file/MDbrew-1.1.1.tar.gz",
+    install_requies=[],
+    description="Postprocessing tools for the MD simulation results (ex. lammps)",
+    packages=find_packages(),
+    keywords=["MD", "LAMMPS"],
+    python_requires=">=3",
+    package_data={},
+    zip_safe=False,
+)
