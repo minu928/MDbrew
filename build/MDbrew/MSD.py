fa5526d1209d8ba573bb9fed2d4dd65489067922
MyKnu <k1alty@naver.com>
2022-09-08 10:42:21 +0900

    minw928

---
+++ build/MDbrew/MSD.py
@@ -0,0 +1,135 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class MSD(object):
+    def __init__(self) -> None:
+        self.axis_dict = {"lag": 0, "N_particle": 1, "pos": -1}
+        self.msd_data = 0
+
+    def get_msd(self, position: np.ndarray, method="window", fft=True) -> np.ndarray:
+        """Get MSD
+
+        Calculate the msd data and return it with method and fft
+
+        Args:
+            position (np.ndarray):  Data of Particle's position in each lag time
+            method (str, optional): default = 'window' (window or direct)
+            fft (bool, optional): default = True
+
+        Returns:
+            np.ndarray: _description_
+        """
+        self.position = np.asarray(position, dtype=float)
+        self.msd_data = 0
+
+        if method == "direct":
+            self.msd_data = self.__get_msd_direct()
+        elif method == "window":
+            if fft:
+                self.msd_data = self.__get_msd_fft()
+            else:
+                self.msd_data = self.__get_msd_window()
+        else:
+            print(f"method can be (direct / window) : your note {method} is wrong")
+
+        return self.msd_data
+
+    # Direct method
+    def __get_msd_direct(self) -> list[float]:
+        """MSD - Direct Method
+
+        Calculate the MSD list with linear loop with numpy function
+
+        Time complexity : O(N**2)
+
+        Args:
+            position (np.ndarray): Data of Particle's position in each lag time
+                - shape = [Number of lag, Number of particle, Coordinate data]
+
+        Returns:
+            list[float]: MSD data of each lag time
+        """
+
+        def cal_msd(diff_pos):
+            return np.square(diff_pos).sum(axis=self.axis_dict["N_particle"]).mean()
+
+        initial_position = self.position[0]
+        msd = [
+            cal_msd(np.subtract(current_position, initial_position))
+            for current_position in self.position
+        ]
+        return msd
+
+    # Window method with non-FFT
+    def __get_msd_window(self):
+        """MSD - Window Method with non-FFT
+
+        Calculate the MSD list with linear loop with numpy function
+
+        Time complexity : O(N**2)
+
+        Args:
+            position (np.ndarray): Data of Particle's position in each lag time
+                - shape = [Number of lag, Number of particle, Coordinate data]
+
+        Returns:
+            list[float]: MSD data of each lag time
+        """
+        N, N_particle = self.position.shape[:2]
+        msd_list = np.zeros((N, N_particle))
+        for lag in np.arange(1, N):
+            diff = self.position[lag:] - self.position[:-lag]
+            sqdist = np.square(diff).sum(axis=self.axis_dict["pos"])
+            msd_list[lag, :] = np.mean(sqdist, axis=self.axis_dict["lag"])
+        return msd_list.mean(axis=self.axis_dict["N_particle"])
+
+    # Window method with FFT
+    def __get_msd_fft(self):
+        """MSD - Window method wit FFT
+
+        Calculate the MSD list with linear loop with numpy function
+
+        Time complexity : O(N logN)
+
+        Args:
+            position (np.ndarray): Data of Particle's position in each lag time
+                - shape = [Number of lag, Number of particle, Coordinate data]
+
+        Returns:
+            list[float]: MSD data of each lag time
+        """
+        N, N_particle = self.position.shape[:2]
+
+        D = np.square(self.position).sum(axis=self.axis_dict["pos"])
+        D = np.append(D, np.zeros((N, N_particle)), axis=self.axis_dict["lag"])
+        Q = 2 * np.sum(D, axis=self.axis_dict["lag"])
+        S_1 = np.zeros((N, N_particle))
+        for m in range(N):
+            Q -= D[m - 1, :] + D[N - m, :]
+            S_1[m, :] = Q / (N - m)
+
+        S_2 = self.__auto_correlation()
+        return np.subtract(S_1, 2 * S_2).mean(axis=self.axis_dict["N_particle"])
+
+    # get S2 for FFT
+    def __auto_correlation(self):
+        N = len(self.position)
+        X = np.fft.fft(self.position, n=2 * N, axis=self.axis_dict["lag"])
+        psd = X * X.conjugate()
+        x = np.fft.ifft(psd, axis=self.axis_dict["lag"])
+        x = x[:N].real
+        x = x.sum(axis=self.axis_dict["pos"])
+        n = np.arange(N, 0, -1)
+        return x / n[:, np.newaxis]
+
+    # plot the data
+    def plot_msd(self, time_step=1, **kwargs):
+        lagtime = len(self.position)
+        if self.msd_data:
+            x = np.range(lagtime)
+            y = self.msd_data
+            plt.plot(x, y, **kwargs)
+            plt.show()
+        else:
+            raise ConnectionError("Please do get_msd first")
---
+++ build/MDbrew/RDF.py
@@ -0,0 +1,143 @@
+import numpy as np
+from tqdm import trange
+import matplotlib.pyplot as plt
+
+# Calculate and Plot the RDF
+class RDF:
+    def __init__(self, a: np.array, b: np.array, system_size: np.array) -> None:
+        """init
+
+        Args:
+            a (np.array): [lag time, N_particle, dim]
+            b (np.array): [lag time, N_particle, dim]
+            box_size (np.array): [[-lx, lx], [-ly, ly], [-lz, lz]]
+        """
+        self.a = np.asarray(a)
+        self.b = np.asarray(b)
+        self.systme_size = np.asarray(system_size)[:, 1]
+        self.box_length = self.systme_size * 2
+        self.lag_number = self.a.shape[0]
+        self.a_number = self.a.shape[1]
+        self.b_number = self.b.shape[1]
+        self.b_number_density = self.b_number / np.prod(self.box_length)
+        self.BAR_LENGTH = 70
+
+    # Main function for Get rdf from a and b, [lag time, N_particle, dim]
+    def get_rdf_all(self, resolution: int = 200):
+        self.resolution = resolution
+        self.r_max = np.min(self.systme_size)
+        self.dr = self.r_max / resolution
+        self.histo_method = self.__check_method()
+        self.hist_data = np.zeros(self.resolution)
+
+        for lag in trange(self.lag_number, ncols=self.BAR_LENGTH):
+            # print(f" {lag} / {self.lag_number-1}")
+            self.unit_a = self.a[lag, :, :]
+            self.unit_b = self.b[lag, :, :]
+            self.histo_method()
+        self.g_r = self.__cal_g_r()
+        return self.g_r
+
+    # Function for get radii data
+    def get_radii(self) -> np.ndarray:
+        self.radii = np.linspace(0, self.r_max, self.resolution)
+        return self.radii
+
+    # Function for get coordinate number
+    def get_CN(self) -> np.ndarray:
+        self.n = self.hist_data / (self.lag_number * self.a_number)
+        self.cn = np.cumsum(self.n)
+        return self.cn
+
+    # Check a == b
+    def __check_method(self):
+        if self.a is self.b:
+            print("\n\tSingle Case\n")
+            self.count_num = 2
+            return self.__get_hist_single
+        else:
+            print("\n\tBinary Case\n")
+            self.count_num = 1
+            return self.__get_hist_binary
+
+    # Get histogram data in single case
+    def __get_hist_single(self):
+        # for idx, a_position in enumerate(tqdm(self.unit_a[:-1], ncols=self.BAR_LENGTH)):
+        for idx, a_position in enumerate(self.unit_a[:-1]):
+            for b_position in self.unit_b[idx:]:
+                self.__update_hist_data(a_position, b_position)
+
+    # Get histogram data in binary case
+    def __get_hist_binary(self):
+        # for a_position in tqdm(self.unit_a, ncols=self.BAR_LENGTH):
+        for a_position in self.unit_a:
+            for b_position in self.unit_b:
+                self.__update_hist_data(a_position, b_position)
+
+    # Update the hist_data
+    def __update_hist_data(self, a_position: list[float], b_position: list[float]):
+        r = self.__get_distance(a_position, b_position)
+        idx_hist = int(r / self.dr)
+        if 0 < idx_hist < self.resolution:
+            self.hist_data[idx_hist] += self.count_num
+
+    # Get the distance with checking PBC
+    def __get_distance(self, a, b) -> float:
+        """Distance (radius)
+
+        Calculate distance between a & b with Periodic Boundary Layer
+
+        Args:
+            a, b        (np.ndarray) : position that we want to calculate radius
+
+        Returns:
+            float64 : radius between a and b
+        """
+        delta_coordinates = np.abs(np.subtract(a, b))
+        delta_coordinates = self.__check_pbc(delta_coordinates)
+        r = np.sqrt(np.sum(np.square(delta_coordinates)))
+        return r
+
+    # Check the PBC
+    def __check_pbc(self, delta_coordinates) -> np.array:
+        """Check PBC
+
+        Check delta_coordinates has a value that is larger than box_size
+
+        Args:
+            delta_coordinates   (np.ndarray) : distance data between each dimensions
+            box_size            (np.ndarray) : size of simulation
+
+        Returns:
+            np.ndarray : value checked delta_coordinates with Periodic Boudary Layer
+        """
+        for dim, box_length in enumerate(self.box_length):
+            if delta_coordinates[dim] > self.systme_size[dim]:
+                delta_coordinates[dim] = box_length - delta_coordinates[dim]
+        return delta_coordinates
+
+    # Calculate the Density Function
+    def __cal_g_r(self):
+        g_r = np.zeros(self.resolution)
+        for i in np.arange(1, self.resolution):
+            r_i = i * self.dr
+            g_r[i] = self.hist_data[i] / (r_i * r_i)
+        self.factor = 4 * np.pi * self.b_number_density * self.dr
+        denominator = self.factor * self.lag_number * self.a_number
+        return g_r / denominator
+
+    # Plot the g(r) with radii data
+    def plot_g_r(self, *args, **kwrgs):
+        try:
+            plt.plot(self.get_radii(), self.g_r, *args, **kwrgs)
+            plt.xlabel("r")
+            plt.ylabel("g(r)")
+            plt.hlines(1.0, 0, self.r_max, colors="red", linestyles="--")
+            plt.plot()
+        except:
+            raise Exception("get_g_r first")
+
+    # Set the BAR_LENGTH
+    def set_BAR_LENGTH(self, width=70):
+        self.BAR_LENGTH = width
+        print(f"Progress bar length is changed into {width}")
