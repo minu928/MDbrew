4472d008f1576600eec992e3f5571ba38523b311
MyKnu <color.ui>
2022-09-11 20:37:35 +0900

    update version 1.3.6

--- package/build/lib/MDbrew/tester.py
+++ package/build/lib/MDbrew/tester.py
@@ -7,10 +7,12 @@ from .MSD import MSD
 def tester(path):
     print("\n\ttest Init \u2713 \t\n")
     data = DumpOpener(path)
-    pos = Extractor(data=data).get_position_db(type_=1)[-2:]
+    extractor = Extractor(data=data)
+    pos = extractor.get_position_db(type_=1)[-10:]
+    uw_pos = extractor.get_unwrapped_position(type_=1)[-10:]
     ss = data.get_system_size()
     data.get_columns()
     data.get_time_step()
     RDF(pos, pos, ss).get_rdf()
-    MSD(pos).get_msd()
+    MSD(uw_pos).get_msd()
     print("\n\ttest Done \u2713 \t\n")
