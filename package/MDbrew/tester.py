ae503f69d523fa86d76fa732376736b1e9f25613
MyKnu <color.ui>
2022-09-13 22:46:01 +0900

    Update Version 1.4.0

--- package/MDbrew/tester.py
+++ package/MDbrew/tester.py
@@ -1,7 +1,7 @@
 from .opener import DumpOpener
-from .Extractor import Extractor
-from .RDF import RDF
-from .MSD import MSD
+from .extractor import Extractor
+from .rdf import RDF
+from .msd import MSD
 
 
 def tester(path):
@@ -12,6 +12,6 @@ def tester(path):
     system_size = extractor.system_size
     position = extractor.extract_position(type_=1, wrapped=True)[-10:]
     uw_position = extractor.extract_position(type_=1, wrapped=False)[-10:]
-    RDF(position, position, system_size).get_rdf()
+    RDF(position, position, system_size).get_rdf(r_max=10)
     MSD(uw_position).get_msd()
     print("\n\ttest Done \u2713 \t\n")
--- package/build/lib/MDbrew/rdf.py
+++ package/build/lib/MDbrew/rdf.py
@@ -21,22 +21,35 @@ class RDF:
         self.b_number = self.b.shape[1]
 
     # Main function for Get rdf from a and b, [lag time, N_particle, dim]
-    def get_rdf(self, resolution: int = 10000):
+    def get_rdf(self, r_max: float, resolution: int = 10000, pbc="single"):
+        """get RDF
+
+        Function for calculate the rdf.
+
+        Args:
+            r_max (float): write down your max radius
+            resolution (int, optional): set the resolution. Defaults to 10000.
+            pbc (str, optional): single -> one system, layer -> 27 system . Defaults to "single".
+
+        Returns:
+            NDArray : data of g_r
+        """
+        self.r_max = r_max
         self.resolution = resolution
-        self.r_max = np.min(self.system_size).astype(np.float64)
-        self.dr = (self.r_max / self.resolution).astype(np.float64)
+        self.dr = float(self.r_max / self.resolution)
         self.hist_data = np.zeros(self.resolution)
+        self.__check_pbc = self.__set_pbc_style(pbc=pbc)
         kwrgs_trange = {"desc": " RDF  (STEP) ", "ncols": 70, "ascii": True}
         for lag in trange(self.lag_number, **kwrgs_trange):
             self.a_unit = self.a[lag, :, :].astype(np.float64)
             self.b_unit = self.b[lag, :, :].astype(np.float64)
             self.__make_histogram()
-        self.g_r = self.__cal_g_r()
+        self.g_r = self.__get_g_r()
         return self.g_r
 
     # Function for get radii data
     def get_radii(self) -> np.ndarray:
-        self.radii = np.linspace(0, self.r_max, self.resolution)
+        self.radii = np.linspace(0.0, self.r_max, self.resolution)
         return self.radii
 
     # Function for get coordinate number
@@ -48,20 +61,51 @@ class RDF:
     # make a histogram
     def __make_histogram(self):
         for b_line in self.b_unit:
-            b_tile = np.tile(b_line, (self.a_number, 1))
-            diff_position = np.abs(np.subtract(self.a_unit, b_tile, dtype=np.float64))
-            diff_position = self.__check_PBC(diff_position=diff_position)
+            diff_position = self.__get_diff_position(target=b_line)
+            diff_position = self.__check_pbc(diff_position=diff_position)
             distance = self.__get_distance(diff_position=diff_position)
             idx_hist = self.__get_idx_histogram(distance=distance)
-            self.hist_data[idx_hist] += 1.0
+            value, count = np.unique(idx_hist, return_counts=True)
+            self.hist_data[value] += count
 
-    # check the diiferent of position
-    def __check_PBC(self, diff_position):
+    # get position difference
+    def __get_diff_position(self, target):
+        return np.abs(np.subtract(self.a_unit, target, dtype=np.float64))
+
+    def __set_pbc_style(self, pbc):
+        if pbc == "single":
+            return self.__set_pbc_single
+        elif pbc == "layer":
+            self.layer = self.__make_first_layer()
+            return self.__set_pbc_layer
+        else:
+            raise ValueError(f"pbc <- (single) or (layer) : your Value {pbc} is wrong")
+
+    # set the pbc only consider single system
+    def __set_pbc_single(self, diff_position):
         return np.where(
             diff_position > self.system_size,
             self.box_length - diff_position,
             diff_position,
         )
+
+    # set the pbc with 27 system
+    def __set_pbc_layer(self, diff_position):
+        return diff_position[:, np.newaxis, :] + self.layer
+
+    # Make a first layer
+    def __make_first_layer(self):
+        return self.__make_direction_idx() * self.box_length
+
+    # Make a 3D layer_idx
+    def __make_direction_idx(self):
+        list_direction = []
+        idx_direction_ = [-1, 0, 1]
+        for i in idx_direction_:
+            for j in idx_direction_:
+                for k in idx_direction_:
+                    list_direction.append([i, j, k])
+        return np.array(list_direction)
 
     # get distance from different of position
     def __get_distance(self, diff_position):
@@ -73,21 +117,25 @@ class RDF:
         return idx_hist[np.where((0 < idx_hist) & (idx_hist < self.resolution))]
 
     # Calculate the Density Function
-    def __cal_g_r(self):
+    def __get_g_r(self):
         r_i = self.get_radii()[1:]
         g_r = np.append(0.0, self.hist_data[1:] / np.square(r_i))
-        factor = np.array(4.0 * np.pi * self.b_number * self.dr, dtype=np.float64)
-        factor *= np.array(self.lag_number * self.a_number, dtype=np.float64)
+        factor = np.array(
+            4.0 * np.pi * self.dr * self.lag_number * self.a_number * self.b_number,
+            dtype=np.float64,
+        )
         box_volume = np.prod(self.box_length, dtype=np.float64)
         return g_r * box_volume / factor
 
-    # Plot the g(r) with radii data
-    def plot_g_r(self, *args, **kwrgs):
+    # Plot the g(r) with radii datas
+    def plot_g_r(self, bins: int = 1, *args, **kwrgs):
         try:
-            plt.plot(self.radii, self.g_r, *args, **kwrgs)
+            x = self.radii[::bins]
+            y = self.g_r[::bins]
+            plt.plot(x, y, *args, **kwrgs)
             plt.xlabel("r")
             plt.ylabel("g(r)")
-            plt.hlines(1.0, 0, self.r_max, colors="red", linestyles="--")
+            plt.hlines(1.0, 0, self.r_max + 1, colors="black", linestyles="--")
             plt.plot()
         except:
             raise Exception("get_g_r first")
