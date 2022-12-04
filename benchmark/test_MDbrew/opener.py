22e431b80312acd377b230c543250c3f4efe10ed
MyKnu <color.ui>
2022-12-04 22:17:57 +0900

    change_hierchy_of_Opener

--- benchmark/test_MDbrew/opener.py
+++ benchmark/test_MDbrew/opener.py
@@ -28,6 +28,16 @@ class Opener(object):
             raise Exception("We cannot change string to float in each data")
         return line
 
+    # # find the idx list of start point
+    def _find_idxlist_by_word(self, word: str) -> list[int]:
+        return [idx for idx, line in enumerate(self.lines) if word in line]
+
+    # find the first idx
+    def _find_idx_by_word(self, word: str) -> int:
+        for idx, line in enumerate(self.lines):
+            if word in line:
+                return idx
+
 
 # 2nd Generation -> For "dump.lammpstrj"
 class DumpOpener(Opener):
@@ -40,18 +50,24 @@ class DumpOpener(Opener):
 
         Args:
             file_path   (str)       :   file path of dump.lammpstrj
-            target_info (list[str]) :   List with string, 0 = target_line, 1 = target_word
+            target_info (list[str]) :   List with string, target_line = "id", target_word = "NUMBER"
 
+        Example:
+            >>> data        = DumpOpener(file_path)
+            >>> database    = data.get_database
+            >>> columns     = data.get_columns
+            >>> system_size = data.get_system_size
+            >>> time_step   = data.get_time_step
         """
         super().__init__(file_path)
-        self.lines: list = super().get_lines()
-        self.target_info = self.set_target_info(target_info=target_info)
-        self.target_line: str = self.target_info[0]
-        self.target_word: str = self.target_info[1]
-        self.system_num: int = self.__get_system_num()
-        self.start_idx_list: list[int] = self.__find_word_idx_list(
-            word=self.target_line
+        self.lines: list[str] = super().get_lines()
+        target_info = self.__set_target_info(target_info)
+        self.target_line = target_info[0]
+        self.target_word = target_info[1]
+        self.system_num: int = int(
+            self.lines[super()._find_idx_by_word(self.target_word) + 1]
         )
+        self.start_idx_list: list[int] = super()._find_idxlist_by_word(self.target_line)
 
     # Get the database from a, b
     @timeCount
@@ -60,64 +76,34 @@ class DumpOpener(Opener):
         for idx in self.start_idx_list:
             start_idx: int = idx + 1
             end_idx: int = start_idx + self.system_num
-            lines = self.lines[start_idx:end_idx]
-            lines = super().seperate_data_in_lines(lines=lines)
-            database.append(lines)
+            data = self.lines[start_idx:end_idx]
+            data = super().seperate_data_in_lines(lines=data)
+            database.append(data)
         return database
 
     # Find the columns data in lines
     @timeCount
-    def get_columns(self) -> list[str]:
-        for line in self.lines:
-            if self.target_line in line:
-                columns = line.split(" ")
-                columns = self.__remove_other(columns)
-                return columns
-        return []
+    def get_columns(self, erase: int = 2) -> list[str]:
+        first_idx: int = self.start_idx_list[0]
+        return self.lines[first_idx].split(" ")[erase:]
 
     # find the system size
     @timeCount
-    def get_system_size(self, dim=3, word="BOX") -> list[float]:
-        size_idx = self.__find_word_idx(word=word) + 1
+    def get_system_size(self, dim: int = 3, word="BOX") -> list[float]:
+        size_idx = super()._find_idx_by_word(word=word) + 1
         system_size = self.lines[size_idx : size_idx + dim]
         system_size = super().seperate_data_in_lines(lines=system_size)
         return system_size
 
     # find the time step
     @timeCount
-    def get_time_step(self) -> list[float]:
-        time_step_idx_list = self.__find_word_idx_list(word="TIMESTEP")
+    def get_time_step(self, word: str = "TIMESTEP") -> list[float]:
+        time_step_idx_list = super()._find_idxlist_by_word(word=word)
         time_step_list = [int(self.lines[idx + 1]) for idx in time_step_idx_list]
         return time_step_list
 
-    # Remove the word, ITEM:
-    def __remove_other(self, line: list[str]) -> list[str]:
-        if "ITEM:" in line:
-            return line[2:]
-        else:
-            return line
-
-    # # find the idx list of start point
-    def __find_word_idx_list(self, word: str) -> list[int]:
-        return [idx for idx, line in enumerate(self.lines) if word in line]
-
-    # find the first idx
-    def __find_word_idx(self, word: str) -> int:
-        for idx, line in enumerate(self.lines):
-            if word in line:
-                return idx
-
-    # Fine the system num
-    def __get_system_num(self) -> int:
-        for idx, line in enumerate(self.lines):
-            if self.target_word in line:
-                system_num = self.lines[idx + 1]
-                system_num = int(system_num)
-                return system_num
-        return 0
-
     # Set target_information
-    def set_target_info(self, target_info):
+    def __set_target_info(self, target_info):
         if target_info == None:
             return ["id", "NUMBER"]
         else:
