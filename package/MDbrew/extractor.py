c46895656bb77e9a16988e3b65a379f99dcfb9ff
MyKnu <k1alty@naver.com>
2022-12-07 23:23:20 +0900

    Version 1.4.8

--- package/MDbrew/extractor.py
+++ package/MDbrew/extractor.py
@@ -6,21 +6,22 @@ from .opener import Opener
 
 # Extract the data
 class Extractor(object):
-    def __init__(self, opener: Opener, pos_: list[str] = None) -> None:
+    def __init__(self, opener: Opener, dim: int = 3) -> None:
+        self.dim = dim
         self.database = opener.get_database()
-        self.column = opener.get_columns()
+        self.columns = opener.get_columns()
         self.system_size = opener.get_system_size()
         self.time_step = opener.get_time_step()
         self.lag_number = len(self.database)
-        self.pos_ = self.__check_position(pos_=pos_)
+        self.pos_ = self.__check_position()
 
     @timeCount
-    def extract_position(self, type_: int, wrapped=True) -> NDArray[np.float64]:
+    def extract_position(self, type: int, wrapped=True) -> NDArray[np.float64]:
         db_position = []
         get_position = self.__check_method(wrapped=wrapped)
         for lag in range(self.lag_number):
-            df_data = pd.DataFrame(data=self.database[lag], columns=self.column)
-            self.df_data = df_data[df_data["type"] == type_]
+            df_data = pd.DataFrame(data=self.database[lag], columns=self.columns)
+            self.df_data = df_data[df_data["type"] == type]
             position = get_position()
             db_position.append(position)
         return np.asarray(db_position, dtype=np.float64)
@@ -31,19 +32,25 @@ class Extractor(object):
         else:
             return self.__df_unwrapped_position
 
-    def __df_wrapped_position(self):
+    def __df_wrapped_position(self) -> NDArray[np.float64]:
         return np.array(self.df_data[self.pos_])
 
     def __df_unwrapped_position(self) -> NDArray[np.float64]:
-        if self.pos_ == ["xu", "yu", "zu"] or self.pos_ == ["xsu", "ysu", "zsu"]:
-            return np.array(self.__df_wrapped_position())
+        if self.already_unwrapped:
+            return self.__df_wrapped_position()
         else:
+            idx_ix = self.columns.index("ix")
+            list_in = self.columns[idx_ix : idx_ix + self.dim]
             box_size = np.array(self.system_size)[:, 1]
-            idx_position = self.df_data[["ix", "iy", "iz"]] * box_size
-            return np.array(idx_position) + np.array(self.__df_wrapped_position())
+            idx_position = self.df_data[list_in] * box_size
+            return np.array(idx_position) + self.__df_wrapped_position()
 
-    def __check_position(self, pos_) -> list[str]:
-        if pos_ == None:
-            return ["x", "y", "z"]
-        else:
-            return pos_
+    def __check_position(self) -> list[str]:
+        for idx, column in enumerate(self.columns):
+            if column in ["x", "xs"]:
+                self.already_unwrapped = False
+                return self.columns[idx : idx + self.dim]
+            elif column in ["xu", "xsu"]:
+                self.already_unwrapped = True
+                return self.columns[idx : idx + self.dim]
+        raise Exception(f"COLUMNS : {self.columns} is not normal case")
--- package/MDbrew/tester.py
+++ package/MDbrew/tester.py
@@ -9,8 +9,8 @@ def tester(path):
     opener = DumpOpener(path)
     extractor = Extractor(opener=opener)
     system_size = extractor.system_size
-    position = extractor.extract_position(type_=1, wrapped=True)[-10:]
-    uw_position = extractor.extract_position(type_=1, wrapped=False)[-10:]
+    position = extractor.extract_position(type=1, wrapped=True)[-10:]
+    uw_position = extractor.extract_position(type=1, wrapped=False)[-10:]
     RDF(position, position, system_size).get_rdf(r_max=10)
     MSD(uw_position).get_msd()
     print("\n\ttest Done \u2713 \t\n")
