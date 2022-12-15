f276ca7f82b63a29f1811cc63e01b9a212fe2b5f
MyKnu <color.ui>
2022-12-15 20:08:45 +0900

    make type

--- benchmark/test.py
+++ benchmark/test.py
@@ -1,5 +1,7 @@
+import test_MDbrew as mdb
 from test_MDbrew.tool.tester import do_test
 
 if __name__ == "__main__":
     path = "./test.lammpstrj"
+    print(f"\n{' '*6}Version : {mdb.__version__}")
     do_test(path=path)
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
+import MDbrew as mdb
 from MDbrew.tool.tester import do_test
 
-path = "./benchmark/test.lammpstrj"
-do_test(path)
+if __name__ == "__main__":
+    path = "./benchmark/test.lammpstrj"
+    print(f"\n{' '*6}Version : {mdb.__version__}")
+    do_test(path=path)
