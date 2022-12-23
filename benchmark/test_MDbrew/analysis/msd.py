cb0092bad2db32978d0347feccf3842cd5155aa0
MyKnu <color.ui>
2022-12-23 23:36:17 +0900

    lag time to frame

--- benchmark/test_MDbrew/analysis/msd.py
+++ benchmark/test_MDbrew/analysis/msd.py
@@ -12,14 +12,14 @@ class MSD(object):
         Calculate the msd data and return it with method and fft
 
         Args:
-            position (np.ndarray)   :  Data of Particle's position in each lag time
+            position (np.ndarray)   :  Data of Particle's position in each frame
             fft (bool, optional)    :  default = True
 
         ## Result of 'Mean Square Displacement'
         >>> my_msd      = MSD(position = position, fft = true)
         >>> msd_result  = my_msd.result
         """
-        self.axis_dict = {"lag": 0, "N_particle": 1, "pos": -1}
+        self.axis_dict = {"frame": 0, "N_particle": 1, "pos": -1}
         self.position = spacer.check_dimension(position, dim=3)
         self.kwrgs_it = {"desc": " MSD  (STEP) ", "ncols": 70, "ascii": True}
         self.N = self.position.shape[0]
@@ -48,17 +48,17 @@ class MSD(object):
         Time complexity : O(N**2)
 
         Args:
-            position (np.ndarray): Data of Particle's position in each lag time
-                - shape = [Number of lag, Number of particle, Coordinate data]
+            position (np.ndarray): Data of Particle's position in each frame
+                - shape = [Number of frame, Number of particle, Coordinate data]
 
         Returns:
-            list[float]: MSD data of each lag time
+            list[float]: MSD data of each frame
         """
         msd_list = np.zeros(self.position.shape[:2])
-        for lag in trange(1, self.N, **self.kwrgs_it):
-            diff_position = spacer.get_diff_position(self.position[lag:], self.position[:-lag])
+        for frame in trange(1, self.N, **self.kwrgs_it):
+            diff_position = spacer.get_diff_position(self.position[frame:], self.position[:-frame])
             distance = self.__square_sum_position(diff_position)
-            msd_list[lag, :] = np.mean(distance, axis=self.axis_dict["lag"])
+            msd_list[frame, :] = np.mean(distance, axis=self.axis_dict["frame"])
         return self.__mean_msd_list(msd_list=msd_list)
 
     # window method with FFT
@@ -70,11 +70,11 @@ class MSD(object):
         Time complexity : O(N logN)
 
         Args:
-            position (np.ndarray): Data of Particle's position in each lag time
-                - shape = [Number of lag, Number of particle, Coordinate data]
+            position (np.ndarray): Data of Particle's position in each frame
+                - shape = [Number of frame, Number of particle, Coordinate data]
 
         Returns:
-            list[float]: MSD data of each lag time
+            list[float]: MSD data of each frame
         """
         S_1 = self.__get_S_1()
         S_2 = self.__get_S_2()
@@ -84,8 +84,8 @@ class MSD(object):
     def __get_S_1(self) -> NDArray[np.float64]:
         empty_matrix = np.zeros(self.position.shape[:2])
         D = self.__square_sum_position(self.position)
-        D = np.append(D, empty_matrix, axis=self.axis_dict["lag"])
-        Q = 2.0 * np.sum(D, axis=self.axis_dict["lag"])
+        D = np.append(D, empty_matrix, axis=self.axis_dict["frame"])
+        Q = 2.0 * np.sum(D, axis=self.axis_dict["frame"])
         S_1 = empty_matrix
         for m in trange(self.N, **self.kwrgs_it):
             Q -= D[m - 1, :] + D[self.N - m, :]
@@ -94,9 +94,9 @@ class MSD(object):
 
     # get S2 for FFT
     def __get_S_2(self) -> NDArray[np.float64]:
-        X = np.fft.fft(self.position, n=2 * self.N, axis=self.axis_dict["lag"])
+        X = np.fft.fft(self.position, n=2 * self.N, axis=self.axis_dict["frame"])
         dot_X = X * X.conjugate()
-        x = np.fft.ifft(dot_X, axis=self.axis_dict["lag"])
+        x = np.fft.ifft(dot_X, axis=self.axis_dict["frame"])
         x = x[: self.N].real
         x = x.sum(axis=self.axis_dict["pos"])
         n = np.arange(self.N, 0, -1)
@@ -112,8 +112,8 @@ class MSD(object):
 
     # plot the data
     def plot_result(self, time_step: float = 1, *args, **kwargs):
-        lagtime = len(self.position)
-        x = np.arange(0, lagtime * time_step, time_step)
+        frames = len(self.position)
+        x = np.arange(0, frames * time_step, time_step)
         y = self.result
         plt.plot(x, y, *args, **kwargs)
         plt.show()
