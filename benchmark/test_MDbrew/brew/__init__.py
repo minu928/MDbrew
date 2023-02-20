1439e953567494fe01cc9ee589b4756b92e526a4
MyKnu <k1alty@naver.com>
2023-02-20 12:14:09 +0900

    add DatOpener

--- benchmark/test_MDbrew/brew/__init__.py
+++ benchmark/test_MDbrew/brew/__init__.py
@@ -1,4 +1,4 @@
 from .extractor import Extractor
-from .opener import LAMMPSOpener, Opener, GromacsOpener
+from .opener import LAMMPSOpener, Opener, DatOpener
 
-__all__ = ["Extractor", "LAMMPSOpener", "Opener", "GromacsOpener"]
+__all__ = ["Extractor", "LAMMPSOpener", "Opener", "DatOpener"]
