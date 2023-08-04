ad97a6fa00bc18afc9d9c8464b245d38d98c5e56
MyKnu <k1alty@naver.com>
2023-08-04 12:12:40 +0900

    add PeriodicCKDTree

--- benchmark/sudo_MDbrew/spatial/spacer.py
+++ benchmark/sudo_MDbrew/spatial/spacer.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-__all__ = ["check_dimension", "get_diff_position", "get_distance", "wrap_position", "unwrap_position"]
+__all__ = ["check_dimension", "get_diff_position", "get_distance", "wrap_position", "unwrap_position", "calculate_angle_between_vectors"]
 
 
 # Dimension checker
@@ -20,6 +20,14 @@ def get_distance(diff_position, axis: int = -1, dtype: str = float):
     return np.sqrt(np.sum(np.square(diff_position), axis=axis)).astype(dtype)
 
 
+# calculate the angle between vectors
+def calculate_angle_between_vectors(v1, v2):
+    dot_product = np.sum(v1 * v2)
+    norm_v1 = np.linalg.norm(v1)
+    norm_v2 = np.linalg.norm(v2)
+    return np.arccos(dot_product / (norm_v1 * norm_v2)) * 180.0 / np.pi
+
+
 # wrap the position data
 def wrap_position(position, box):
     box = np.asarray(box, dtype=float)
@@ -29,9 +37,17 @@ def wrap_position(position, box):
 
 
 # unwrap the position data
-def unwrap_position(position, pre_postion, box):
+def unwrap_position(position, pre_position, box, ixyz=None, return_ixyz=False):
     box = np.asarray(box, dtype=float)
-    delta_position = position - pre_postion
-    position = np.where(delta_position < box * 0.5, position + box, position)
-    position = np.where(delta_position > box * 0.5, position - box, position)
-    return position
+    if ixyz is None:
+        ixyz = np.zeros(position.shape)
+    
+    delta_position = position - pre_position
+    ixyz += np.where(delta_position < -box * 0.5, 1, 0)
+    ixyz -= np.where(delta_position > box * 0.5, 1, 0)
+    position += ixyz * box
+
+    if return_ixyz:
+        return position, ixyz
+    else:
+        return position
